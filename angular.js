be771528ca9d2b621af5ed5426761ba4992d971c
Fábio Englert Moutinho <fabioemoutinho@gmail.com>
2022-07-27 21:19:11 -0300

    fix: angular and rxjs configurations in monorepos (#7)

--- angular.js
+++ angular.js
@@ -18,42 +18,4 @@ module.exports = {
       },
     ],
   },
-  // angular defaults
-  overrides: [
-    {
-      files: ["*.ts"],
-      parserOptions: {
-        project: ["tsconfig.json"],
-        createDefaultProgram: true,
-      },
-      plugins: ["@angular-eslint"],
-      extends: [
-        "plugin:@angular-eslint/recommended",
-        "plugin:@angular-eslint/template/process-inline-templates",
-      ],
-      rules: {
-        "@angular-eslint/directive-selector": [
-          "error",
-          {
-            type: "attribute",
-            prefix: "app",
-            style: "camelCase",
-          },
-        ],
-        "@angular-eslint/component-selector": [
-          "error",
-          {
-            type: "element",
-            prefix: "app",
-            style: "kebab-case",
-          },
-        ],
-      },
-    },
-    {
-      files: ["*.html"],
-      extends: ["plugin:@angular-eslint/template/recommended"],
-      rules: {},
-    },
-  ],
 };
