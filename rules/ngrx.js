8353fff70795d83653f17843062c1963416ce35d
fabioemoutinho <fabioemoutinho@gmail.com>
2022-08-24 14:19:47 -0300

    fix ngrx config and support new ngrx plugin

--- rules/ngrx.js
+++ rules/ngrx.js
@@ -1,16 +1,39 @@
 const tryRequire = require("../lib/try-require");
-const ngrxStore = tryRequire("@ngrx/store");
-const ngrxComponent = tryRequire("@ngrx/component-store");
-const ngrx = ngrxStore || ngrxComponent;
+const ngrxStore = tryRequire("@ngrx/store/package.json");
+const ngrxComponentStore = tryRequire("@ngrx/component-store/package.json");
+const ngrx = ngrxStore || ngrxComponentStore;
+
+const getPackageMajorVersion = (package) => {
+  if (!package) {
+    return;
+  }
+  return parseInt(package.version.split(".")[0]);
+};
 
 module.exports = !ngrx
   ? {}
-  : {
-      overrides: [
-        {
-          files: ["*.ts"],
-          plugins: ["ngrx"],
-          extends: ["plugin:ngrx/recommended"],
-        },
-      ],
-    };
+  : (() => {
+      const ngrxStoreVersion = getPackageMajorVersion(ngrxStore);
+      const ngrxComponentStoreVersion =
+        getPackageMajorVersion(ngrxComponentStore);
+
+      if (ngrxStoreVersion < 14 || ngrxComponentStoreVersion < 14) {
+        return {
+          overrides: [
+            {
+              files: ["*.ts"],
+              plugins: ["ngrx"],
+              extends: ["plugin:ngrx/recommended"],
+            },
+          ],
+        };
+      }
+      return {
+        overrides: [
+          {
+            files: ["*.ts"],
+            extends: ["plugin:@ngrx/recommended"],
+          },
+        ],
+      };
+    })();
